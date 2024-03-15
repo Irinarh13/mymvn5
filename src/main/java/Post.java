67fa3e240eafcc3ae09da4a937d5cdfa2477ffa0
Irinarh <jagdtiger_86@mail.ru>
2024-03-15 15:14:12 -0700

    Initial commit

---
+++ src/main/java/Post.java
@@ -0,0 +1,15 @@
+public class Post {
+    public String name; // поле для имени
+    public String passport; // поле для паспорта
+    public String patronymic; // поле для отчества
+    public String phone; // поле для телефона
+    public String surname; // поле для фамилии
+    public boolean subscription; // подтверждающее поле
+    public FormDate birthday;
+
+
+}
+
+
+
+
